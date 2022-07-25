7bfd8724d378e0d87e4eb369091d5edf1ae862a9
Kunamo <98980709+Kunamo@users.noreply.github.com>
2022-07-26 01:47:29 +0200

    finished get started example

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
+// background.js --> Handle background services
+
+// Set variable
 let color = '#3aa757';
 
+// On installed:
 chrome.runtime.onInstalled.addListener(() => {
+    // Put in storage
     chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+
+    // Output on console DEBUGGING
+    console.log('%cThis is the color', `This is the color: ${color}`);
 })
