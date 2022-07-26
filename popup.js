5584dc7efbf2901ce0771f35feec3926866d5107
Kunamo <98980709+Kunamo@users.noreply.github.com>
2022-07-26 02:15:55 +0200

    can create new elements on button click

--- popup.js
+++ popup.js
@@ -1,25 +1,25 @@
-// Button in html to variable
-let changeColor = document.getElementById("changeColor");
+// html button to js
+let button = document.getElementById("activate");
 
 // Get the variable called "color" from storage //TODO: Don't fully understand syntax yet.
-chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
-});
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+button.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: setPageBackgroundColor,
+        function: newP(),
     });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
+
+function newP() {
+    const para = document.createElement("p");
+    const node = document.createTextNode("This is a paragraph.");
+    para.appendChild(node);
+
+    const element = document.body;
+    element.appendChild(para);
 }
